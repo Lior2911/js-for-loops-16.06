dcda69ab41e80477793fd5c8ec4482bfd0a9fe83
lior2911 <aytegev1@gmail.com>
2022-06-16 16:09:04 +0300

    2commit

--- main.js
+++ main.js
@@ -3,11 +3,27 @@
 //   console.log(someElement)
 // }
 
-let pargrraphColor = document.getElementsByTagName("p");
+// let pargrraphColor = document.getElementsByTagName("p");
 
-function changeColor(){
-  for (const correntP of pargrraphColor){
-    correntP.innerText = "hello";
-    correntP.style.color = "blue";
-  }
-}
+// function changeColor(){
+//   for (const correntP of pargrraphColor){
+//     correntP.innerText = "hello";
+//     correntP.style.color = "blue";
+//   }
+// }
+
+// let printLine = document.getElementById("printLine");
+// let someObject = {
+//   firstName : "lior",
+//   lastName : "aytegev",
+//   age : "31",
+//   phoneNumber: "0523278300",
+// }
+// function printFromObject(){
+
+//   for(const keyInObj in someObject){
+//     console.log(keyInObj)
+//     printLine.innerHTML += `${keyInObj}:${someObject[keyInObj]}`+"<br>"
+//   }
+// }
+// printFromObject();
